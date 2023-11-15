99cc8d70ecea3ce00363b72374fda55646c0fd07
PranavBarthwal <pbarthwal90@gmail.com>
2023-11-16 01:14:08 +0530

    progress

--- 14.1Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ 14.1Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,56 +1,71 @@
+//DETECTS BUTTON PRESS
 var noOfButtons = document.querySelectorAll(".drum").length;
 
+for (var i=0 ; i<noOfButtons ; i++){
+document.querySelectorAll("button")[i].addEventListener("click", function(){
+    
+    var button = this.innerHTML;
 
-for(var i=0 ; i<noOfButtons ; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function(){
-        
-        var buttonInnerHTML = this.innerHTML;
+    playSound(button);
 
-        switch(buttonInnerHTML){
-            
-            case "w":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-            break;
- 
-            case "a":
-                var kick = new Audio("sounds/kick-bass.mp3");
-                kick.play();
-            break;
-
-            case "s":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-            break;
-
-            case "d":
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-            break;
-
-            case "j":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-            break;
-
-            case "k":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-            break;
-
-            case "l":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-            break;
-
-            default:
-                break;
-        }
-    });
+});
 }
 
 
+//DETECTS KEYBOARD PRESS
+document.addEventListener("keydown", function(event){
+    playSound(event.key);
+})
+
+
+//PLAYS SOUND
+function playSound(key){
+
+    switch(key){
+
+          case "w":
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+          break; 
+          
+          case "a":
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+          break;    
+
+          case "s":
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+          break;
+
+          case "d":
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+          break;
+
+          case "j":
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+          break;
+
+          case "k":
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+          break;
+
+          case "l":
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+          break;
+
+          default : console.log(this.innerHTML);
+          break;
+ 
+
+        }
 
 
 
+}
 
+ 
